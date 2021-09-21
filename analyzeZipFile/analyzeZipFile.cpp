0e75cd88457659c0168895050818e9167c65ddfb
sunday-robot <sgx03150@nifty.com>
2021-09-21 14:52:08 +0900

    a

--- analyzeZipFile/analyzeZipFile.cpp
+++ analyzeZipFile/analyzeZipFile.cpp
@@ -1,5 +1,6 @@
 ﻿#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void printKey(const char *key) {
 	printf("  %-60s:", key);
@@ -32,24 +33,37 @@ void printQWord(const char *key, unsigned long long value) {
 	printf("0x%016llx\n", value);
 }
 
-void printShort(const char *key, short value) {
+void printUShort(const char *key, unsigned short value) {
 	printKey(key);
-	printf("%d(2)\n", value);
+	if (value == USHRT_MAX) {
+		printf("-(2)\n");
+	}
+	else {
+		printf("%u(2)\n", value);
+	}
 }
 
-void printLong(const char *key, long value) {
+void printULong(const char *key, unsigned long value) {
 	printKey(key);
-	printf("%d(4)\n", value);
+	if (value == ULONG_MAX) {
+		printf("-(4)\n");
+	}
+	else {
+		printf("%u(4)\n", value);
+	}
 }
 
-void printLongLong(const char *key, long long value) {
+void printULongLong(const char *key, unsigned long long value) {
 	printKey(key);
-	printf("%lld(8)\n", value);
+	if (value == ULLONG_MAX) {
+		printf("-(8)\n");
+	}
+	else {
+		printf("%llu(8)\n", value);
+	}
 }
 
-//-------------------------------------------
-
-void readAndPrintString(const char *key, FILE *fp, int size) {
+void readAndPrintString(const char *key, FILE *fp, unsigned int size) {
 	printKey(key);
 	auto *buf = new unsigned char[size + 1];
 	fread(buf, 1, size, fp);
@@ -86,45 +100,43 @@ unsigned long long readAndPrintQWord(const char *key, FILE *fp) {
 	return value;
 }
 
-short readAndPrintShort(const char *key, FILE *fp) {
-	short value;
+unsigned short readAndPrintUShort(const char *key, FILE *fp) {
+	unsigned short value;
 	fread(&value, 2, 1, fp);
-	printShort(key, value);
+	printUShort(key, value);
 	return value;
 }
 
-long readAndPrintLong(const char *key, FILE *fp) {
-	long value;
+unsigned long readAndPrintULong(const char *key, FILE *fp) {
+	unsigned long value;
 	fread(&value, 4, 1, fp);
-	printLong(key, value);
+	printULong(key, value);
 	return value;
 }
 
-long long readPrintLongLong(const char *key, FILE *fp) {
-	long long value;
+unsigned long long readAndPrintULongLong(const char *key, FILE *fp) {
+	unsigned long long value;
 	fread(&value, 8, 1, fp);
-	printLongLong(key, value);
+	printULongLong(key, value);
 	return value;
 }
-
-//-------------------------------------------
 
 void printZip64ExtraField(const unsigned char *data, int dataSize,
 	bool originalSizeRequired, bool compressedSizeRequired, bool offsetOflocalFileHeaderRequired,
-	long long *compressedSize, unsigned long long *offsetOfLocalFileHeader)
+	unsigned long long *compressedSize, unsigned long long *offsetOfLocalFileHeader)
 {
 	int index = 0;
 	if (originalSizeRequired) {
-		auto value = *((long long *)(data + index));
-		printLongLong("      original size", value);
+		auto value = *((unsigned long long *)(data + index));
+		printULongLong("      original size", value);
 		index += 8;
 		if (index >= dataSize)
 			return;
 	}
 
 	if (compressedSizeRequired) {
-		auto value = *((long long *)(data + index));
-		printLongLong("      compressed size", value);
+		auto value = *((unsigned long long *)(data + index));
+		printULongLong("      compressed size", value);
 		if (compressedSize != 0)
 			*compressedSize = value;
 		index += 8;
@@ -142,8 +154,8 @@ void printZip64ExtraField(const unsigned char *data, int dataSize,
 			return;
 	}
 
-	auto value = *((long *)(data + index));
-	printLong("      disk start number", value);
+	auto value = *((unsigned long *)(data + index));
+	printULong("      disk start number", value);
 }
 
 void printNtfsExtraField(const unsigned char *data, int dataSize) {
@@ -151,14 +163,14 @@ void printNtfsExtraField(const unsigned char *data, int dataSize) {
 	printByte("      Reserved", p, 4); p += 4;
 	int index = 0;
 	while (p < data + dataSize) {
-		auto tag = *((short *)p); p += 2;
-		auto size = *((short *)p); p += 2;
+		auto tag = *((unsigned short *)p); p += 2;
+		auto size = *((unsigned short *)p); p += 2;
 		char section[100];
 		sprintf_s(section, "      [%d]", index);
 		printKey(section);
 		printf("\n");
 		printWord("        Tag", tag);
-		printShort("        Size", size);
+		printUShort("        Size", size);
 		printByte("        Mtime", p, 8); p += 8;
 		printByte("        Atime", p, 8); p += 8;
 		printByte("        Ctime", p, 8); p += 8;
@@ -167,7 +179,7 @@ void printNtfsExtraField(const unsigned char *data, int dataSize) {
 
 void analyzeExtraField(FILE *fp, int size,
 	bool originalSizeRequired, bool compressedSizeRequired, bool offsetOflocalFileHeaderRequired,
-	long long *compressedSize, unsigned long long *offsetOfLocalFileHeader) {
+	unsigned long long *compressedSize, unsigned long long *offsetOfLocalFileHeader) {
 	printKey("extra field");
 	if (size == 0) {
 		printf("(empty)\n");
@@ -181,9 +193,9 @@ void analyzeExtraField(FILE *fp, int size,
 		do {
 			printf("    [%d]\n", index++);
 			auto headerId = *((unsigned short*)p); p += 2;
-			auto dataSize = *((short*)p); p += 2;
+			auto dataSize = *((unsigned short*)p); p += 2;
 			printWord("    header ID", headerId);
-			printShort("    data size", dataSize);
+			printUShort("    data size", dataSize);
 			switch (headerId) {
 			case 0x0001:
 				printf("      data(ZIP64)\n");
@@ -213,22 +225,22 @@ void printOffsetAndSignature(FILE *fp, const char *section, int fileIndex = -1) 
 
 void analyzeLocalFileHeader(FILE *fp, int fileIndex, unsigned short *generalPurposeFlag) {
 	printOffsetAndSignature(fp, "local file header", fileIndex);
-	readAndPrintShort("version needed to extract", fp);
+	readAndPrintUShort("version needed to extract", fp);
 	*generalPurposeFlag = readAndPrintWord("general purpose bit flag", fp);
-	readAndPrintShort("compression method", fp);
+	readAndPrintUShort("compression method", fp);
 	readAndPrintByte("last mod file time", fp, 2);
 	readAndPrintByte("last mod file date", fp, 2);
 	readAndPrintDWord("crc-32", fp);
-	auto compressedSize = readAndPrintLong("compressed size", fp);
-	auto uncompressedSize = readAndPrintLong("uncompressed size", fp);
-	auto fileNameLength = readAndPrintShort("file name length", fp);
-	auto extraFieldLength = readAndPrintShort("extra field length", fp);
+	auto compressedSize = readAndPrintULong("compressed size", fp);
+	auto uncompressedSize = readAndPrintULong("uncompressed size", fp);
+	auto fileNameLength = readAndPrintUShort("file name length", fp);
+	auto extraFieldLength = readAndPrintUShort("extra field length", fp);
 	readAndPrintString("file name", fp, fileNameLength);
-	analyzeExtraField(fp, extraFieldLength, compressedSize == -1, uncompressedSize == -1, false, 0, 0);
+	analyzeExtraField(fp, extraFieldLength, compressedSize == ULONG_MAX, uncompressedSize == ULONG_MAX, false, 0, 0);
 	printf("\n");
 }
 
-void analyzeDataDescriptor(short version, FILE *fp, int fileIndex) {
+void analyzeDataDescriptor(unsigned short version, FILE *fp, int fileIndex) {
 #if false
 	auto offset = _ftelli64(fp);
 	printf("%016llx: data descriptor[%d]\n", offset, fileIndex);
@@ -238,39 +250,39 @@ void analyzeDataDescriptor(short version, FILE *fp, int fileIndex) {
 
 	readAndPrintDWord("crc-32", fp);
 	if (version == 45) {
-		readPrintLongLong("compressed size", fp);
-		readPrintLongLong("uncompressed size", fp);
+		readAndPrintULongLong("compressed size", fp);
+		readAndPrintULongLong("uncompressed size", fp);
 	}
 	else {
-		readAndPrintLong("compressed size", fp);
-		readAndPrintLong("uncompressed size", fp);
+		readAndPrintULong("compressed size", fp);
+		readAndPrintULong("uncompressed size", fp);
 	}
 	printf("\n");
 }
 
-long long analyzeCentralDirectoryHeader(FILE *fp, int fileIndex, short *version, long long *compressedSize, unsigned long long *offsetOfLocalFileHeader) {
+unsigned long long analyzeCentralDirectoryHeader(FILE *fp, int fileIndex, unsigned short *version, unsigned long long *compressedSize, unsigned long long *offsetOfLocalFileHeader) {
 	auto start = _ftelli64(fp);
 
 	printOffsetAndSignature(fp, "central directory header", fileIndex);
-	readAndPrintShort("version made by", fp);
-	*version = readAndPrintShort("version needed to extract", fp);
+	readAndPrintUShort("version made by", fp);
+	*version = readAndPrintUShort("version needed to extract", fp);
 	readAndPrintWord("general purpose bit flag", fp);
-	readAndPrintShort("compression method", fp);
+	readAndPrintUShort("compression method", fp);
 	readAndPrintByte("last mod file time", fp, 2);
 	readAndPrintByte("last mod file date", fp, 2);
 	readAndPrintDWord("crc-32", fp);
-	*compressedSize = readAndPrintLong("compressed size", fp);
-	auto uncompressedSize = readAndPrintLong("uncompressed size", fp);
-	auto fileNameLength = readAndPrintShort("file name length", fp);
-	auto extraFieldLength = readAndPrintShort("extra field length", fp);
-	auto fileCommentLength = readAndPrintShort("file comment length", fp);
-	readAndPrintShort("disk number start", fp);
-	readAndPrintShort("internal file attribures", fp);
-	readAndPrintLong("extranal file attributes", fp);
+	*compressedSize = readAndPrintULong("compressed size", fp);
+	auto uncompressedSize = readAndPrintULong("uncompressed size", fp);
+	auto fileNameLength = readAndPrintUShort("file name length", fp);
+	auto extraFieldLength = readAndPrintUShort("extra field length", fp);
+	auto fileCommentLength = readAndPrintUShort("file comment length", fp);
+	readAndPrintUShort("disk number start", fp);
+	readAndPrintWord("internal file attribures", fp);
+	readAndPrintDWord("extranal file attributes", fp);
 	*offsetOfLocalFileHeader = readAndPrintDWord("relative offset of local header", fp);
 	readAndPrintString("file name", fp, fileNameLength);
 	analyzeExtraField(fp, extraFieldLength,
-		uncompressedSize == -1, *compressedSize == -1, *offsetOfLocalFileHeader == 0xffffffff,
+		uncompressedSize == ULONG_MAX, *compressedSize == ULONG_MAX, *offsetOfLocalFileHeader == ULONG_MAX,
 		compressedSize, offsetOfLocalFileHeader);
 	readAndPrintString("file comment", fp, fileCommentLength);
 	printf("\n");
@@ -278,29 +290,29 @@ long long analyzeCentralDirectoryHeader(FILE *fp, int fileIndex, short *version,
 	return _ftelli64(fp) - start;
 }
 
-void analyzeEndOfCentralDirectoryRecord(FILE *fp, long long *sizeOfTheCentralDirectory, unsigned long long *offsetOfStartOfCentralDirectory) {
+void analyzeEndOfCentralDirectoryRecord(FILE *fp, unsigned long long *sizeOfTheCentralDirectory, unsigned long long *offsetOfStartOfCentralDirectory) {
 	printOffsetAndSignature(fp, "end of central directory record");
-	readAndPrintShort("number of this disk", fp);
-	readAndPrintShort("number of the disk with the start of the central directory", fp);
-	readAndPrintShort("total number of entries in the central directory on this disk", fp);
-	readAndPrintShort("total number of entries in the central directory", fp);
-	*sizeOfTheCentralDirectory = readAndPrintLong("size of the central directory", fp);
+	readAndPrintUShort("number of this disk", fp);
+	readAndPrintUShort("number of the disk with the start of the central directory", fp);
+	readAndPrintUShort("total number of entries in the central directory on this disk", fp);
+	readAndPrintUShort("total number of entries in the central directory", fp);
+	*sizeOfTheCentralDirectory = readAndPrintULong("size of the central directory", fp);
 	*offsetOfStartOfCentralDirectory = readAndPrintDWord("offset of start of central directory with respect to the starting disk number", fp);
-	auto zipFileCommentLength = readAndPrintShort(".ZIP file comment length", fp);
+	auto zipFileCommentLength = readAndPrintUShort(".ZIP file comment length", fp);
 	readAndPrintString(".ZIP file comment", fp, zipFileCommentLength);
 	printf("\n");
 }
 
-void analyzeZip64EndOfCentralDirectoryRecord(FILE *fp, long long *sizeOfTheCentralDirectory, unsigned long long *offsetOfStartOfCentralDirectory) {
+void analyzeZip64EndOfCentralDirectoryRecord(FILE *fp, unsigned long long *sizeOfTheCentralDirectory, unsigned long long *offsetOfStartOfCentralDirectory) {
 	printOffsetAndSignature(fp, "Zip64 end of central directory record");
-	auto size = readPrintLongLong("size of zip64 end of central directory record", fp);
-	readAndPrintShort("version made by", fp);
-	readAndPrintShort("version need to extract", fp);
-	readAndPrintLong("number of this disk", fp);
-	readAndPrintLong("number of the disk with the start of the central directory", fp);
-	readPrintLongLong("total number of entries in the central directory on this disk", fp);
-	readPrintLongLong("total number of entries in the central directory", fp);
-	*sizeOfTheCentralDirectory = readPrintLongLong("size of the central directory", fp);
+	auto size = readAndPrintULongLong("size of zip64 end of central directory record", fp);
+	readAndPrintUShort("version made by", fp);
+	readAndPrintUShort("version need to extract", fp);
+	readAndPrintULong("number of this disk", fp);
+	readAndPrintULong("number of the disk with the start of the central directory", fp);
+	readAndPrintULongLong("total number of entries in the central directory on this disk", fp);
+	readAndPrintULongLong("total number of entries in the central directory", fp);
+	*sizeOfTheCentralDirectory = readAndPrintULongLong("size of the central directory", fp);
 	*offsetOfStartOfCentralDirectory = readAndPrintQWord("offset of start of central directory with respect to the starting disk number", fp);
 	auto zedsSize = size - (2 + 2 + 4 + 4 + 8 + 8 + 8 + 8);	// "version make..."から、"offset of start..."のバイト数を差し引くと、"zip64 extensible..."のバイト数が得られる。
 	readAndPrintByte("zip64 extensible data sector", fp, (int)zedsSize);
@@ -309,9 +321,9 @@ void analyzeZip64EndOfCentralDirectoryRecord(FILE *fp, long long *sizeOfTheCentr
 
 void analyzeZip64EndOfCentralDirectoryLocator(FILE *fp, unsigned long long *offsetOfTheZip64EndOfCentralDirectoryRecord) {
 	printOffsetAndSignature(fp, "Zip64 end of central directory locator");
-	readAndPrintLong("number of the disk with the start of the zip64 end central directory", fp);
+	readAndPrintULong("number of the disk with the start of the zip64 end central directory", fp);
 	*offsetOfTheZip64EndOfCentralDirectoryRecord = readAndPrintQWord("relative offset of the zip64 end of central directory record", fp);
-	readAndPrintLong("total number of disks", fp);
+	readAndPrintULong("total number of disks", fp);
 	printf("\n");
 }
 
@@ -321,7 +333,7 @@ int main(int argc, char *argv[])
 	fopen_s(&fp, argv[1], "rb");
 
 	// end of central directory recordは22バイト固定ではなく、はZIPコメントの長さで変わるが、本プログラムではZIPコメントはないものと決め打ちしている。
-	long long sizeOfTheCentralDirectory;
+	unsigned long long sizeOfTheCentralDirectory;
 	unsigned long long offsetOfStartOfCentralDirectory;
 	_fseeki64(fp, -22, SEEK_END);
 	analyzeEndOfCentralDirectoryRecord(fp, &sizeOfTheCentralDirectory, &offsetOfStartOfCentralDirectory);
@@ -337,8 +349,8 @@ int main(int argc, char *argv[])
 	}
 
 	for (auto i = 0; sizeOfTheCentralDirectory > 0; i++) {
-		short version;
-		long long compressedSize;
+		unsigned short version;
+		unsigned long long compressedSize;
 		unsigned long long offsetOfLocalFileHeader;
 		_fseeki64(fp, offsetOfStartOfCentralDirectory, SEEK_SET);
 		sizeOfTheCentralDirectory -= analyzeCentralDirectoryHeader(fp, i, &version, &compressedSize, &offsetOfLocalFileHeader);
